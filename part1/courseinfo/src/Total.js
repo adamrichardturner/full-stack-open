154dd081caad3842ea815b8cad0b1be2eb4c5877
Adam Turner <hello@adamrichardturner.dev>
2023-03-01 20:42:46 +0000

    Completed exercise 1.5

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,6 +1,12 @@
 const Total = props => {
     return (
-        <p>Number of exercises {props.total}</p>
+        <p>
+            Number of exercises {
+            props.parts[0].exercises + 
+            props.parts[1].exercises + 
+            props.parts[2].exercises
+            }
+        </p>
     )
 }
 
