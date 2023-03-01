e7de8c912d73975ce3eb5abfeb90a59c86fc6594
Adam Turner <hello@adamrichardturner.dev>
2023-03-01 20:38:29 +0000

    Completed exercise 1.4

--- part1/courseinfo/src/Part.js
+++ part1/courseinfo/src/Part.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Part = props => {
     return (
         <p>
-            {props.part} {props.exercise}
+            {props.part.name} {props.part.exercises}
         </p>
     )
 }
