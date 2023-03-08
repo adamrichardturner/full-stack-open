5a9f64a63e7274f542b2f48d2b81758bd1c11cfa
Adam Turner <hello@adamrichardturner.dev>
2023-03-08 07:03:12 +0000

    Completed exercise 1.7 unicafe step2

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,8 +1,16 @@
 const Statistics = ({good, neutral, bad}) => {
+    const badPoints = bad * -1
     return (
         <>
           <h2>statistics</h2>
-          <p>good {good}<br />neutral {neutral}<br />bad {bad}</p>
+          <p>
+            good {good}<br />
+            neutral {neutral}<br />
+            bad {bad}<br />
+            all {good + neutral + bad}<br />
+            average {(good + badPoints) / 2}<br />
+            positive {(good / (good + neutral + bad)) * 100}%
+          </p>
         </>
     )
 }
