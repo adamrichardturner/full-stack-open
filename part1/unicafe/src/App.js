9077a400eec2ceb53db02346aea9b88c7587ee7f
Adam Turner <hello@adamrichardturner.dev>
2023-03-08 07:42:22 +0000

    Completed exercise 1.12* anecdotes step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,19 +4,36 @@ import Button from './Button'
 import Statistics from './Statistics'
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const handleAnecdote = e => {
+    const rand = Math.floor(Math.random() * anecdotes.length)
+    console.log(rand)
+    return setSelected(rand)
+  }
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleButtonState = (event) => {
-    if(event.target.name === "good") {
+  const handleButtonState = (e) => {
+    if(e.target.name === "good") {
       const count = good + 1
       setGood(count)
-    } else if (event.target.name === "neutral") {
+    } else if (e.target.name === "neutral") {
       const count = neutral + 1
       setNeutral(count)
-    } else if (event.target.name === "bad") {
+    } else if (e.target.name === "bad") {
       const count = bad + 1
       setBad(count)
     }
@@ -35,6 +52,10 @@ const App = () => {
       </Button>
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <div>
+        {anecdotes[selected]}<br />
+        <button onClick={handleAnecdote}>next anecdote</button>
+      </div>
     </>
   )
 }
