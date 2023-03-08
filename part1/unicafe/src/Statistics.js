ef2258eb349d57664f4ae2b542fb2c17bf0c2205
Adam Turner <hello@adamrichardturner.dev>
2023-03-08 07:56:52 +0000

    Completed unicafe exercises and refactored

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,7 +1,9 @@
 import StatisticsLine from "./StatisticsLine"
 
 const Statistics = ({good, neutral, bad}) => {
+    // badPoints stores our bad points for average calculation
     const badPoints = bad * -1
+    // conditional render if no button has been pressed
     if(good === 0 && neutral === 0 && bad === 0) {
         return(<p>No feedback given</p>)
     } else {
