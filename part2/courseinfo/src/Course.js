062b4447ea77bf12dd91c778f9715751152cc5ec
Adam Turner <hello@adamrichardturner.dev>
2023-03-10 21:05:05 +0000

    Completed exercises 2.2 on courseinfo step7

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -3,10 +3,13 @@ import Content from "./Content"
 
 const Course = ({ course }) => {
     const { name, parts } = course;
+    const exercises = parts.map(part => part.exercises)
+    const total = exercises.reduce((a, b) => a + b)
     return(
         <>
             <Header name={name}/>
             <Content parts={parts}/>
+            <p style={{fontWeight: 800}}>total of {total} exercises</p>
         </>
     )
 }
