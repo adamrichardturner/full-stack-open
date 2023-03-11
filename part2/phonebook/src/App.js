a5b9aea21f9dd31bab13a1616a0f7a04c92783e2
Adam Turner <hello@adamrichardturner.dev>
2023-03-11 07:43:08 +0000

    Completed exercise 2.8 phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,17 +3,27 @@ import { useState } from 'react'
 const App = () => {
   // Manage persons in phonebook state here
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-1234567' 
+    }
   ]) 
   // Manage newName additions here
   const [newName, setNewName] = useState('')
   // Handle the form input for adding a name
-  const handleName = (e) => {
+  const handleName = e => {
     const name = e.target.value
     setNewName(name)
   }
+  // Manage newNumber additions here
+  const [newNumber, setNewNumber] = useState('')
+  // Handle the form input for adding a number
+  const handleNumber = e => {
+    const number = e.target.value
+    setNewNumber(number)
+  }
   // Handle submit of the form here
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault()
     // If newName is in persons, add it to an array in nameExists
     const nameExists = persons.filter(person => person.name === newName)
@@ -21,7 +31,7 @@ const App = () => {
     // add it to the persons array in state
     if(nameExists.length === 0) {
       // Store in newPerson an object to be added to persons state array
-      const newPerson = { name: newName }
+      const newPerson = { name: newName, number: newNumber }
       // Update persons array with newPerson concatenated to it
       setPersons(prevPersons =>(prevPersons.concat(newPerson)))
       // Reset newName state to empty string to reflect form submission
@@ -35,7 +45,7 @@ const App = () => {
   // Store in displayPersons an array of persons
   const displayPersons = persons.map(person => (
                             <p key={person.name}style={{margin: 0}}>
-                              {person.name}
+                              {person.name} {person.number}
                             </p>)
                           )
 
@@ -45,6 +55,9 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleName}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
