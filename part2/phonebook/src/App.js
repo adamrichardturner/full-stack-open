298ba33349f8ab26d5c7938780e49ab3bb48160b
Adam Turner <hello@adamrichardturner.dev>
2023-03-11 08:11:54 +0000

    Completed exercise 2.9* phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,10 @@ import { useState } from 'react'
 const App = () => {
   // Manage persons in phonebook state here
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567' 
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   // Manage newName additions here
   const [newName, setNewName] = useState('')
@@ -22,6 +22,13 @@ const App = () => {
     const number = e.target.value
     setNewNumber(number)
   }
+  // Manage search state here
+  const [searchTerm, setSearchTerm] = useState('')
+  // Handle the search input
+  const handleSearch = e => {
+    const term = e.target.value
+    setSearchTerm(term.toLowerCase())
+  }
   // Handle submit of the form here
   const handleSubmit = e => {
     e.preventDefault()
@@ -30,8 +37,13 @@ const App = () => {
     // If nameExists has a length of 0, this is a unique name, we should
     // add it to the persons array in state
     if(nameExists.length === 0) {
+      // Get all ids from persons
+      const ids = persons.map(person => person.id)
+      // Generate unique id for new person object by adding 1 to the max
+      // of the existing ids
+      const newId = Math.max(...ids) + 1
       // Store in newPerson an object to be added to persons state array
-      const newPerson = { name: newName, number: newNumber }
+      const newPerson = { name: newName, number: newNumber, id: newId }
       // Update persons array with newPerson concatenated to it
       setPersons(prevPersons =>(prevPersons.concat(newPerson)))
       // Reset newName state to empty string to reflect form submission
@@ -42,16 +54,21 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     }
   }
-  // Store in displayPersons an array of persons
-  const displayPersons = persons.map(person => (
-                            <p key={person.name}style={{margin: 0}}>
-                              {person.name} {person.number}
-                            </p>)
-                          )
+  // Store in personsToShow an array of persons with name and number
+  // If searchTerm is false, return just the persons array
+  // Else filter it to only include persons with names including searchTerm
+  const personsToShow = !searchTerm
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(searchTerm))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        <p>filter shown with </p>
+        <input value={searchTerm} onChange={handleSearch}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleName}/>
@@ -64,7 +81,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {displayPersons}
+      {personsToShow.map(person => (<p key={person.id}>{person.name} {person.number}</p>))}
     </div>
   )
 }
