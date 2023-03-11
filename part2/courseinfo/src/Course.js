1d2d4a162f5db10c6dde995e41fd677bf9378710
Adam Turner <hello@adamrichardturner.dev>
2023-03-11 07:33:35 +0000

    Added notes

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -2,8 +2,11 @@ import Header from "./Header"
 import Content from "./Content"
 
 const Course = ({ course }) => {
+    // Destructure course into name and parts variables
     const { name, parts } = course;
+    // Store in exercises an array of the exercises for each part
     const exercises = parts.map(part => part.exercises)
+    // Store in total the total of all exercises
     const total = exercises.reduce((a, b) => a + b)
     return(
         <>
