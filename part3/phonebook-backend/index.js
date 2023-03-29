b68a1fbf6d5180cf850ba29ed42127e0091fc8a3
Adam Turner <hello@adamrichardturner.dev>
2023-03-29 16:14:28 +0100

    Completed exercise 3.17*: Phonebook database, step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -86,6 +86,21 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
     // Delete a person from the phonebook
     Person.findByIdAndRemove(request.params.id)
