39f3603954eb48968f93f428256870944d2b6793
Adam Turner <hello@adamrichardturner.dev>
2023-04-01 21:51:36 +0100

    Completed exercise 4.2 Blog list, step2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,44 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+// Define route to handle GET requests to /api/blogs, which retrieves all blog data from the MongoDB database
+blogsRouter.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+// Define route to handle POST requests to /api/blogs, which adds a new blog to the MongoDB database
+blogsRouter.post('/api/blogs', (request, response) => {
+  // Extract blog data from request body
+  const body = request.body
+
+  // Check that all required fields are present
+  if (body.title === undefined) {
+    return response.status(400).json({ error: 'missing title' })
+  } else if (body.author === undefined) {
+    return response.status(400).json({ error: 'missing author' })
+  } else if (body.url === undefined) {
+    return response.status(400).json({ error: 'missing url' })
+  } else if (body.likes === undefined) {
+    return response.status(400).json({ error: 'missing likes' })
+  }
+
+  // Create a new blog using the extracted data
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  // Save the new blog to the MongoDB database
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result)
+    })
+    .catch((error) => console.log(error))
+})
+
+module.exports = blogsRouter
