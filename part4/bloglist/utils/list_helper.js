0a8bee747a4c43243596659b695576c2e9685537
Adam Turner <hello@adamrichardturner.dev>
2023-04-05 14:07:43 +0100

    Completed exercises 4.5*, 4.6* and 4.7* helper functions and unit tests step3, 4, and 5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -8,7 +10,39 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (max, blog) => {
+      return blog.likes > max.likes
+        ? { title: blog.title, author: blog.author, likes: blog.likes }
+        : max
+    },
+    { title: '', author: '', likes: 0 }
+  )
+}
+
+const mostBlogs = (blogs) => {
+  const freq = _.countBy(blogs, 'author')
+  const author = _.maxBy(_.keys(freq), (author) => freq[author])
+  return { author, blogs: freq[author] }
+}
+
+const mostLikes = (blogs) => {
+  const result = _.chain(blogs)
+    .groupBy('author')
+    .map((blogList, author) => ({
+      author,
+      likes: _.sumBy(blogList, 'likes')
+    }))
+    .maxBy('likes')
+    .value()
+  return result
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
