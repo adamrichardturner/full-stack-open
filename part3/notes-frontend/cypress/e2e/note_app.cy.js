c2b66eb81e4782dbb3b3ecc9e572b4fe0cf330df
Adam Turner <hello@adamrichardturner.dev>
2023-05-27 13:39:18 +0100

    More cypress

--- part3/notes-frontend/cypress/e2e/note_app.cy.js
+++ part3/notes-frontend/cypress/e2e/note_app.cy.js
@@ -42,20 +42,17 @@ describe('Note app', function () {
       cy.contains('a note created by cypress')
     })
 
-    describe('and a note exists', function () {
+    describe('and several notes exist', function () {
       beforeEach(function () {
-        cy.createNote({
-          content: 'another note cypress',
-          important: true
-        })
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
       })
 
-      it('it can be made not important', function () {
-        cy.contains('another note cypress')
-          .contains('make not important')
-          .click()
-
-        cy.contains('another note cypress').contains('make important')
+      it('one of those can be made important', function () {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make not important')
       })
     })
   })
