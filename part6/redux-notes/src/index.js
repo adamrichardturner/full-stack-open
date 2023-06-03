b730edcf777b5c7af82b667926cca216a0529173
adamrichardturner <hello@adamrichardturner.dev>
2023-06-03 20:31:34 +0100

    Further study on Redux

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
+
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 console.log(store.getState())
 
