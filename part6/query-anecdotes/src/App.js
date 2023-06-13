fdab36779348adf037932c4cd7670bc6a8770a24
adamrichardturner <hello@adamrichardturner.dev>
2023-06-13 12:52:46 +0100

    Working on first exercise

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,19 +1,20 @@
+import { useQuery, useMutation } from 'react-query'
+import { getAnecdotes } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
-
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery('notes', getAnecdotes)
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
