46eae6618f227c2c68cd66e6aa66866ba9ca81c1
adamrichardturner <hello@adamrichardturner.dev>
2023-06-16 20:23:16 +0100

    Continued work on part 6 material

---
+++ part6/query-anecdotes/hook-counter/src/components/Display.js
@@ -0,0 +1,11 @@
+import { useCounterValue } from '../CounterContext'
+
+const Display = () => {
+  const counter = useCounterValue()
+  return <div>
+    {counter}
+  </div>
+}
+
+
+export default Display
--- part6/query-anecdotes/hook-counter/src/index.js
+++ part6/query-anecdotes/hook-counter/src/index.js
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import { CounterContextProvider } from './CounterContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
 )
